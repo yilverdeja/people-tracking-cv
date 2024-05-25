411dda7f89ff2337336cbaadcb3e35ab85ebb0c1
Yil Verdeja <yilverdeja@gmail.com>
2024-05-25 15:48:05 +0800

    init: initialize next cv project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	) => {
+		config.resolve.fallback = {
+			fs: false,
+			path: false,
+			crypto: false,
+		};
+
+		return config;
+	},
+};
 
 export default nextConfig;
